bbd54202ebcd3dcb592ffed9afa92ccc9af30ae8
Nacho Serrano <ignaserrano@gmail.com>
2020-11-25 23:28:40 +0100

    Created first structure to read csv

--- src/main/java/org.dis/LeeCSV.java
+++ src/main/java/org.dis/LeeCSV.java
@@ -5,10 +5,31 @@ package org.dis;
 
 // Otros imports ...
 
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
 public class LeeCSV {
 
     public List<golfPlayer> parseaCSV(String path) {
-        List<golfPlayer> players = csvToBean.parse();
-        return players;
+        try {
+            BufferedReader fileReader = new BufferedReader(new FileReader("best-golf-players.csv"));
+
+            CsvToBean<golfPlayer> csvToBean = new CsvToBeanBuilder<golfPlayer>(fileReader) //Lista de beans para cargar el csv<br/>
+                    .withType(golfPlayer.class)
+                    .withIgnoreLeadingWhiteSpace(true) // Para ignorar blancos
+                    .build();
+            List<golfPlayer> players = csvToBean.parse();
+            return players;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
